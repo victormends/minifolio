49f13081d44dee87fc066453453183b8a86fec9d
João Victor Mendes <j.victor8@hotmail.com>
2022-11-18 15:03:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -147,29 +147,35 @@
 </section>
 <!-- portfolio section --> 
 <!-- hire me section -->
+
 <section id="hireme" class="section hireme">
   <div class="container">
     <div class="col-md-8 col-md-offset-2 text-center">
-      <h3>Need something specific?</h3>
-      <p>We are currently crafting new products but would love to hear from you.</p>
-      <a href="#contact" class="btn btn-large">Hire me</a> </div>
-  </div>
+      <?php 
+        if(is_active_sidebar('Hire Me')){
+          dynamic_sidebar('hire-me');
+          }
+      ?>
+    </div>
 </section>
+
+
 <!-- hire me section --> 
-
-
 
 <section id="contact" class="section contact">
   <div class="container">
     <div class="col-md-10 col-md-offset-1 text-center">
-      <h3>Drop me a line</h3>
-      <p>I am honest, discreet and always focus on your goals. If you are looking for a strategist and experienced designer, just leave me a message and I'll contact you soon.</p>
-      <!--contact form start-->
+    <?php 
+          if(is_active_sidebar('Contact')){
+            dynamic_sidebar('contact');
+          }
+        ?>
+    <!--contact form start-->
       <div class="col-md-6 col-md-offset-3 conForm">
         <div id="message"></div>
         <form method="post" action="php/contact.php" name="cform" id="cform">
-          <input name="name" id="name" type="text" class="col-xs-12 col-sm-12 col-md-12 col-lg-12" placeholder="Your name..." >
-          <input name="email" id="email" type="email" class=" col-xs-12 col-sm-12 col-md-12 col-lg-12 noMarr" placeholder="Email Address..." >
+          <input name="name" id="name" type="text" class="col-xs-12 col-sm-12 col-md-12 col-lg-12" placeholder="Your name...">
+          <input name="email" id="email" type="email" class=" col-xs-12 col-sm-12 col-md-12 col-lg-12 noMarr" placeholder="Email Address...">
           <textarea name="comments" id="comments" cols="" rows="" class="col-xs-12 col-sm-12 col-md-12 col-lg-12" placeholder="Project Details..."></textarea>
           <input type="submit" id="submit" name="send" class="submitBnt" value="Send">
           <div id="simple-msg"></div>
@@ -179,6 +185,7 @@
     </div>
   </div>
 </section>
+
 
 
 <!-- footer -->
